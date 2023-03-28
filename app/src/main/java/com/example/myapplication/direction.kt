73840e4c7c0575ca9d267ccb7b2d0fb5cac65efb
Dariika <karasickmalenky@yandex.ru>
2023-03-28 20:39:32 +0300

    added logo and help window

--- app/src/main/java/com/example/myapplication/direction.kt
+++ app/src/main/java/com/example/myapplication/direction.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class direction : AppCompatActivity() {
+
+//    lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.`direction`)
+
+//        text = findViewById(R.id.textview);
+//        // загружаем текст
+//        text.setText("Режим позволяет переключаться между Обычным и Инженерным режимами  Используйте Обычный режим для основных математических операций, Инженерный - для сложных расчетов")
     }
 }
